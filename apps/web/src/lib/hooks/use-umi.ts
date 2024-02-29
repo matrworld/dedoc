127c3ce6d8296519b986d3443c6c7efab5e50533
qudo <qudo@matr.world>
2024-02-28 22:38:53 -0800

    mainnet

--- apps/web/src/lib/hooks/use-umi.ts
+++ apps/web/src/lib/hooks/use-umi.ts
@@ -1,12 +1,11 @@
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters'
 import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
-import { useWallet } from '@solana/wallet-adapter-react'
 import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
 import { irysUploader } from '@metaplex-foundation/umi-uploader-irys';
 
 export const useUmi = (wallet: any) => {
-  const umi = createUmi('https://devnet.helius-rpc.com/?api-key=2cb827e8-a527-4f73-a7b8-15e78ff27e40', { commitment: 'finalized' });
+  const umi = createUmi('https://mainnet.admin6074.workers.dev/', { commitment: 'finalized' });
   umi
     .use(irysUploader({ priceMultiplier: 1.5 }))
     .use(mplTokenMetadata())
