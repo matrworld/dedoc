15de1120efca57c577c47acf4d8e817206070f74
qudo <qudo@matr.world>
2024-02-28 23:13:54 -0800

    new key

--- apps/web/src/lib/hooks/use-umi.ts
+++ apps/web/src/lib/hooks/use-umi.ts
@@ -5,7 +5,7 @@ import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
 import { irysUploader } from '@metaplex-foundation/umi-uploader-irys';
 
 export const useUmi = (wallet: any) => {
-  const umi = createUmi('https://mainnet.admin6074.workers.dev/', { commitment: 'finalized' });
+  const umi = createUmi('https://mainnet.helius-rpc.com/?api-key=2cb827e8-a527-4f73-a7b8-15e78ff27e40', { commitment: 'finalized' });
   umi
     .use(irysUploader({ priceMultiplier: 1.5 }))
     .use(mplTokenMetadata())
